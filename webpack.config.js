f755293ad027b0c629dd8567c3857d8f09ed7999
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-12 01:11:05 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,112 +1,63 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              [
+                "@babel/plugin-transform-react-jsx",
+                { pragma: "h", pragmaFrag: "Fragment" },
+              ],
+            ],
+          },
         },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "responsive-loader",
             options: {
-              name: 'images/[name].[hash:8].[ext]',
-            },
-          },
-          {
-            loader: 'responsive-loader',
-            options: {
-              adapter: require('responsive-loader/sharp'),
+              adapter: require("responsive-loader/sharp"),
               sizes: [300, 600, 900],
-              placeholder: true,
-              placeholderSize: 50,
-              name: '[name]-[width]',
-              outputPath: 'images/',
-              format: 'webp',
-              quality: 70,
+              outputPath: "images",
+              name: "[name]-[width].[ext]",
+              format: "png",
+              quality: 85,
+              esModule: false,
             },
           },
-          {
-            loader: 'responsive-loader',
-            options: {
-              adapter: require('responsive-loader/sharp'),
-              sizes: [300, 600, 900],
-              placeholder: true,
-              placeholderSize: 50,
-              name: '[name]-[width]',
-              outputPath: 'images/',
-              format: 'jpg',
-              quality: 70,
-            },
-          },
-          {
-            loader: 'responsive-loader',
-            options: {
-              adapter: require('responsive-loader/sharp'),
-              sizes: [300, 600, 900],
-              placeholder: true,
-              placeholderSize: 50,
-              name: '[name]-[width]',
-              outputPath: 'images/',
-              format: 'png',
-              quality: 70,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
         ],
       },
     ],
   },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    alias: {
-      'react': 'preact/compat',
-      'react-dom': 'preact/compat'
-    }
-  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'styles.[contenthash].css',
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'public', to: 'public' }
-      ],
+      template: "./src/index.html",
     }),
   ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      react: "preact/compat",
+      "react-dom": "preact/compat",
     },
-    compress: true,
-    port: 8080,
-    hot: true,
-    historyApiFallback: true,
   },
 };
